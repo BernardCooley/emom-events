9bd76172c751e316c36e2e4182e28f7922556037
bernard.cooley <bernard.cooley@global.com>
2024-09-20 21:46:49 +0100

    collapse profile

--- app/components/PromoterProfile.tsx
+++ app/components/PromoterProfile.tsx
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
 import {
-    Box,
     Button,
     Card,
     CardBody,
     CardFooter,
     CardHeader,
+    Collapse,
     Divider,
     Flex,
     Heading,
+    HStack,
+    IconButton,
     SimpleGrid,
     Text,
+    useDisclosure,
     useToast,
     VStack,
 } from "@chakra-ui/react";
 import ImageGrid from "./ImageGrid";
 import PromoterForm from "./PromoterForm";
 import { FirebaseImageBlob, PromoterDetails } from "@/types";
+import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 
 interface Props {
     promoter: PromoterDetails;
@@ -25,8 +29,9 @@ interface Props {
 }
 
 const PromoterProfile = ({ promoter, images, onGetPromoter }: Props) => {
+    const { isOpen, onToggle } = useDisclosure({ defaultIsOpen: true });
     const toast = useToast();
-    const [editing, setEditing] = useState(false);
+    const [isEditing, setEditing] = useState(false);
 
     const getLocation = () => {
         const locationParts = [
@@ -39,81 +44,111 @@ const PromoterProfile = ({ promoter, images, onGetPromoter }: Props) => {
     };
 
     return (
-        <Card w="full">
-            <CardHeader>
-                <Box position="relative">
-                    <Button
-                        onClick={() => setEditing(!editing)}
-                        right={0}
-                        top={0}
-                        variant="link"
-                        position="absolute"
-                    >
-                        {editing ? "Cancel" : "Edit"}
-                    </Button>
-                    <Heading size="md">Profile</Heading>
-                </Box>
-            </CardHeader>
-            <Divider />
-            <CardBody>
-                {!editing && (
-                    <Flex width="full" direction="column" gap={10}>
-                        <SimpleGrid columns={[1, 1, 3]} gap={[6, 6, 0]}>
-                            <VStack alignItems="flex-start">
-                                <Text fontWeight={700}>Name</Text>
-                                <Text>{promoter.name}</Text>
-                            </VStack>
-                            <VStack alignItems="flex-start">
-                                <Text fontWeight={700}>Location</Text>
-                                <Text>{getLocation()}</Text>
-                            </VStack>
-                            <VStack alignItems="flex-start">
-                                <Text fontWeight={700}>Email</Text>
-                                <Text>{promoter.email}</Text>
-                            </VStack>
-                        </SimpleGrid>
-                        <ImageGrid columns={[2, 3, 5, 6, 8]} images={images} />
-                    </Flex>
-                )}
-            </CardBody>
-            <CardFooter>
-                <Flex w="full" alignItems="center" direction="column">
-                    {editing && (
-                        <PromoterForm
-                            existingImages={images}
-                            isEditing={true}
-                            onSuccess={(promoter) => {
-                                if (promoter) {
-                                    toast({
-                                        title: "Your details have been updated.",
-                                        status: "success",
-                                        duration: 5000,
-                                        isClosable: true,
-                                    });
-                                    onGetPromoter();
-                                    setEditing(false);
-                                }
-                            }}
-                            onFail={() => {
-                                toast({
-                                    title: "Failed to update your details. Please try again later.",
-                                    status: "error",
-                                    duration: 5000,
-                                    isClosable: true,
-                                });
-                            }}
-                            defaultValues={{
-                                name: promoter.name || "",
-                                city: promoter.city || "",
-                                state: promoter.state || "",
-                                country: promoter.country || "",
-                                email: promoter.email || "",
-                            }}
-                        />
-                    )}
-                </Flex>
-            </CardFooter>
-        </Card>
+        <>
+            <Collapse in={isOpen} startingHeight={80}>
+                <Card w="full">
+                    <CardHeader>
+                        <HStack
+                            w="full"
+                            alignItems="center"
+                            justifyContent="space-between"
+                        >
+                            <HStack gap={4}>
+                                <Heading w="fit-content" size="md">
+                                    Profile
+                                </Heading>
+                                {!isEditing && (
+                                    <IconButton
+                                        h="28px"
+                                        w="28px"
+                                        minW="unset"
+                                        aria-label="Toggle profile"
+                                        icon={
+                                            isOpen ? (
+                                                <ChevronUpIcon fontSize="28px" />
+                                            ) : (
+                                                <ChevronDownIcon fontSize="28px" />
+                                            )
+                                        }
+                                        onClick={onToggle}
+                                    />
+                                )}
+                            </HStack>
+                            {isOpen && (
+                                <Button
+                                    onClick={() => setEditing(!isEditing)}
+                                    variant="link"
+                                >
+                                    {isEditing ? "Cancel" : "Edit"}
+                                </Button>
+                            )}
+                        </HStack>
+                    </CardHeader>
+                    <Divider />
+                    <CardBody>
+                        {!isEditing && (
+                            <Flex width="full" direction="column" gap={10}>
+                                <SimpleGrid columns={[1, 1, 3]} gap={[6, 6, 0]}>
+                                    <VStack alignItems="flex-start">
+                                        <Text fontWeight={700}>Name</Text>
+                                        <Text>{promoter.name}</Text>
+                                    </VStack>
+                                    <VStack alignItems="flex-start">
+                                        <Text fontWeight={700}>Location</Text>
+                                        <Text>{getLocation()}</Text>
+                                    </VStack>
+                                    <VStack alignItems="flex-start">
+                                        <Text fontWeight={700}>Email</Text>
+                                        <Text>{promoter.email}</Text>
+                                    </VStack>
+                                </SimpleGrid>
+                                <ImageGrid
+                                    columns={[2, 3, 5, 6, 8]}
+                                    images={images}
+                                />
+                            </Flex>
+                        )}
+                    </CardBody>
+                    <CardFooter>
+                        <Flex w="full" alignItems="center" direction="column">
+                            {isEditing && (
+                                <PromoterForm
+                                    existingImages={images}
+                                    isEditing={true}
+                                    onSuccess={(promoter) => {
+                                        if (promoter) {
+                                            toast({
+                                                title: "Your details have been updated.",
+                                                status: "success",
+                                                duration: 5000,
+                                                isClosable: true,
+                                            });
+                                            onGetPromoter();
+                                            setEditing(false);
+                                        }
+                                    }}
+                                    onFail={() => {
+                                        toast({
+                                            title: "Failed to update your details. Please try again later.",
+                                            status: "error",
+                                            duration: 5000,
+                                            isClosable: true,
+                                        });
+                                    }}
+                                    defaultValues={{
+                                        name: promoter.name || "",
+                                        city: promoter.city || "",
+                                        state: promoter.state || "",
+                                        country: promoter.country || "",
+                                        email: promoter.email || "",
+                                    }}
+                                />
+                            )}
+                        </Flex>
+                    </CardFooter>
+                </Card>
+            </Collapse>
+        </>
     );
 };
 
