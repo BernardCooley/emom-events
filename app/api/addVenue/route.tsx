8df75807bb11a99175ce8706367862c5a758b65c
bernard.cooley <bernard.cooley@global.com>
2024-09-26 00:19:14 +0100

    updated

--- app/api/addVenue/route.tsx
+++ app/api/addVenue/route.tsx
@@ -25,13 +25,18 @@ export async function POST(req: Request) {
 
         return response;
     } catch (error: unknown) {
-        console.error("Error creating event:", error);
+        console.error("Error updating promoter:", error);
         if (error instanceof CustomError) {
+            console.log("CustomError");
             return NextResponse.json(
                 { error: error.message },
                 { status: error.status || 500 }
             );
+        } else if (error instanceof Error) {
+            console.log("Error");
+            return NextResponse.json({ error: error.message }, { status: 500 });
         } else {
+            console.log("Unknown error");
             return NextResponse.json(
                 { error: "An unknown error occurred" },
                 { status: 500 }
