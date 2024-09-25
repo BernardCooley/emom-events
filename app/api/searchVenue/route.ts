e92a3f984353fe9411e0095bf59c6fe909300ddb
bernard.cooley <bernard.cooley@global.com>
2024-09-26 00:23:34 +0100

    update

--- app/api/searchVenue/route.ts
+++ app/api/searchVenue/route.ts
@@ -50,23 +50,14 @@ export async function POST(req: Request) {
         });
 
         return response;
-    } catch (error: unknown) {
-        console.error("Error updating promoter:", error);
-        if (error instanceof CustomError) {
-            console.log("CustomError");
-            return NextResponse.json(
-                { error: error.message },
-                { status: error.status || 500 }
-            );
-        } else if (error instanceof Error) {
-            console.log("Error");
-            return NextResponse.json({ error: error.message }, { status: 500 });
-        } else {
-            console.log("Unknown error");
-            return NextResponse.json(
-                { error: "An unknown error occurred" },
-                { status: 500 }
-            );
-        }
+    } catch (error: any) {
+        console.error(error);
+
+        return NextResponse.json(
+            { error: error },
+            {
+                status: error.status || 500,
+            }
+        );
     }
 }
