c26515f99af2a7efdeb3df84525e1c587214f29e
bernard.cooley <bernard.cooley@global.com>
2024-09-26 01:38:51 +0100

    suspense

--- app/(pages)/events/page.tsx
+++ app/(pages)/events/page.tsx
@@ -31,7 +31,7 @@ import {
 } from "@chakra-ui/react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import React, { useEffect, useRef, useState } from "react";
+import React, { Suspense, useEffect, useRef, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { z, ZodType } from "zod";
 import Select from "react-select";
@@ -385,217 +385,220 @@ const Page = ({}: Props) => {
         ) : null;
 
     return (
-        <Flex ref={containerRef} direction="column" gap={2}>
-            <FloatingIconButton
-                icon={
-                    <IconButton
-                        bg="transparent"
-                        fontSize={20}
-                        onClick={() =>
-                            window.scrollTo({
-                                top: 0,
-                                left: 0,
-                                behavior: "smooth",
-                            })
-                        }
-                        aria-label="back to top"
-                        icon={<ArrowRightIcon transform="rotate(270deg)" />}
-                    />
-                }
-                showButton={showToTopButton}
-                positionX="right"
-                positionY="bottom"
-            />
-            <Heading>Events</Heading>
-            <form>
-                <HStack
-                    rounded="lg"
-                    pt={2}
-                    alignItems="flex-end"
-                    justifyContent="flex-end"
-                >
-                    <TextInput
-                        width="180px"
-                        type="date"
-                        title="Date from"
-                        height="40px"
-                        size="md"
-                        name="dateFrom"
-                        control={control}
-                        min={todayDateFormatted}
-                    />
-                    <TextInput
-                        width="180px"
-                        type="date"
-                        title="Date to"
-                        height="40px"
-                        size="md"
-                        name="dateTo"
-                        control={control}
-                        min={watchDateFrom}
-                    />
-                    <Controller
-                        control={control}
-                        name="orderBy"
-                        render={({ field: { ref } }) => (
-                            <Select
-                                placeholder="Sort by"
-                                styles={{
-                                    control: (styles) => ({
-                                        ...styles,
-                                        width: "200px",
-                                    }),
-                                }}
-                                ref={ref}
+        <Suspense fallback={<div>Loading...</div>}>
+            <Flex ref={containerRef} direction="column" gap={2}>
+                <FloatingIconButton
+                    icon={
+                        <IconButton
+                            bg="transparent"
+                            fontSize={20}
+                            onClick={() =>
+                                window.scrollTo({
+                                    top: 0,
+                                    left: 0,
+                                    behavior: "smooth",
+                                })
+                            }
+                            aria-label="back to top"
+                            icon={<ArrowRightIcon transform="rotate(270deg)" />}
+                        />
+                    }
+                    showButton={showToTopButton}
+                    positionX="right"
+                    positionY="bottom"
+                />
+                <Heading>Events</Heading>
+                <form>
+                    <HStack
+                        rounded="lg"
+                        pt={2}
+                        alignItems="flex-end"
+                        justifyContent="flex-end"
+                    >
+                        <TextInput
+                            width="180px"
+                            type="date"
+                            title="Date from"
+                            height="40px"
+                            size="md"
+                            name="dateFrom"
+                            control={control}
+                            min={todayDateFormatted}
+                        />
+                        <TextInput
+                            width="180px"
+                            type="date"
+                            title="Date to"
+                            height="40px"
+                            size="md"
+                            name="dateTo"
+                            control={control}
+                            min={watchDateFrom}
+                        />
+                        <Controller
+                            control={control}
+                            name="orderBy"
+                            render={({ field: { ref } }) => (
+                                <Select
+                                    placeholder="Sort by"
+                                    styles={{
+                                        control: (styles) => ({
+                                            ...styles,
+                                            width: "200px",
+                                        }),
+                                    }}
+                                    ref={ref}
+                                    value={
+                                        options.find(
+                                            (option) =>
+                                                option.value === watchOrderBy
+                                        ) || null
+                                    }
+                                    onChange={(val) =>
+                                        setValue("orderBy", val?.value || "")
+                                    }
+                                    options={options}
+                                />
+                            )}
+                        />
+                        <TextInput
+                            width="full"
+                            onEnter={handleSearch}
+                            placeholder="Search by Title, Venue, Address, Host, or Artist"
+                            type="text"
+                            height="40px"
+                            size="md"
+                            name="searchTerm"
+                            control={control}
+                            rightIcon={
+                                watchSearchTerm.trim().length > 0 ? (
+                                    <HStack mt={0} gap={0} h="38px" mr={10}>
+                                        <IconButton
+                                            h="full"
+                                            bg="transparent"
+                                            aria-label="Search"
+                                            icon={<Search2Icon />}
+                                            onClick={handleSearch}
+                                        />
+                                        <IconButton
+                                            h="full"
+                                            bg="transparent"
+                                            aria-label="Search"
+                                            icon={<CloseIcon />}
+                                            onClick={handleSearchClear}
+                                        />
+                                    </HStack>
+                                ) : null
+                            }
+                        />
+                    </HStack>
+                </form>
+                <Divider />
+                <Box h="24px">
+                    <HStack justifyContent="space-between">
+                        <HStack>
+                            <Text>Showing: </Text>
+                            <Text fontWeight={700}>
+                                {searchParams.get("searchTerm") || "All Events"}{" "}
+                                {events && events.length > 0
+                                    ? `(${events.length})`
+                                    : ""}
+                            </Text>
+                            <ConditionalText
+                                condition={
+                                    watchDateFrom !== todayDateFormatted ||
+                                    watchDateTo.length > 0
+                                }
+                                label="from"
+                                value={watchDateFrom}
+                            />
+                            <ConditionalText
+                                condition={watchDateTo.length > 0}
+                                label="to"
+                                value={watchDateTo}
+                            />
+                            <ConditionalText
+                                condition={watchOrderBy.length > 0}
+                                label="sorted By:"
                                 value={
-                                    options.find(
+                                    (options.find(
                                         (option) =>
                                             option.value === watchOrderBy
-                                    ) || null
+                                    )?.label as string) || ""
                                 }
-                                onChange={(val) =>
-                                    setValue("orderBy", val?.value || "")
-                                }
-                                options={options}
                             />
-                        )}
-                    />
-                    <TextInput
-                        width="full"
-                        onEnter={handleSearch}
-                        placeholder="Search by Title, Venue, Address, Host, or Artist"
-                        type="text"
-                        height="40px"
-                        size="md"
-                        name="searchTerm"
-                        control={control}
-                        rightIcon={
-                            watchSearchTerm.trim().length > 0 ? (
-                                <HStack mt={0} gap={0} h="38px" mr={10}>
-                                    <IconButton
-                                        h="full"
-                                        bg="transparent"
-                                        aria-label="Search"
-                                        icon={<Search2Icon />}
-                                        onClick={handleSearch}
-                                    />
-                                    <IconButton
-                                        h="full"
-                                        bg="transparent"
-                                        aria-label="Search"
-                                        icon={<CloseIcon />}
-                                        onClick={handleSearchClear}
-                                    />
-                                </HStack>
-                            ) : null
-                        }
-                    />
-                </HStack>
-            </form>
-            <Divider />
-            <Box h="24px">
-                <HStack justifyContent="space-between">
-                    <HStack>
-                        <Text>Showing: </Text>
-                        <Text fontWeight={700}>
-                            {searchParams.get("searchTerm") || "All Events"}{" "}
-                            {events && events.length > 0
-                                ? `(${events.length})`
-                                : ""}
-                        </Text>
-                        <ConditionalText
-                            condition={
+                            {(searchParams.get("searchTerm") ||
                                 watchDateFrom !== todayDateFormatted ||
-                                watchDateTo.length > 0
-                            }
-                            label="from"
-                            value={watchDateFrom}
-                        />
-                        <ConditionalText
-                            condition={watchDateTo.length > 0}
-                            label="to"
-                            value={watchDateTo}
-                        />
-                        <ConditionalText
-                            condition={watchOrderBy.length > 0}
-                            label="sorted By:"
-                            value={
-                                (options.find(
-                                    (option) => option.value === watchOrderBy
-                                )?.label as string) || ""
-                            }
-                        />
-                        {(searchParams.get("searchTerm") ||
-                            watchDateFrom !== todayDateFormatted ||
-                            watchOrderBy.length > 0 ||
-                            watchDateTo.length > 0) && (
+                                watchOrderBy.length > 0 ||
+                                watchDateTo.length > 0) && (
+                                <Button
+                                    ml={6}
+                                    variant="link"
+                                    onClick={handleClearAll}
+                                >
+                                    Clear
+                                </Button>
+                            )}
+                        </HStack>
+                        {events && events.length > 0 && (
                             <Button
-                                ml={6}
+                                onClick={() => {
+                                    if (isMapShowing) {
+                                        setIsMapShowing(false);
+                                        removeQueryParams(
+                                            ["showMap"],
+                                            pathname,
+                                            searchParams,
+                                            router
+                                        );
+                                    } else {
+                                        setQueryParams(
+                                            { showMap: ["true"] },
+                                            pathname,
+                                            searchParams,
+                                            router
+                                        );
+                                        setIsMapShowing(true);
+                                    }
+                                }}
                                 variant="link"
-                                onClick={handleClearAll}
                             >
-                                Clear
+                                {isMapShowing && events && events.length > 0
+                                    ? "Show list"
+                                    : "Show on map"}
                             </Button>
                         )}
                     </HStack>
-                    {events && events.length > 0 && (
-                        <Button
-                            onClick={() => {
-                                if (isMapShowing) {
-                                    setIsMapShowing(false);
-                                    removeQueryParams(
-                                        ["showMap"],
-                                        pathname,
-                                        searchParams,
-                                        router
-                                    );
-                                } else {
-                                    setQueryParams(
-                                        { showMap: ["true"] },
-                                        pathname,
-                                        searchParams,
-                                        router
-                                    );
-                                    setIsMapShowing(true);
-                                }
-                            }}
-                            variant="link"
-                        >
-                            {isMapShowing && events && events.length > 0
-                                ? "Show list"
-                                : "Show on map"}
-                        </Button>
-                    )}
-                </HStack>
-            </Box>
-            {!isMapShowing ? (
-                <ItemList page="events" events={events} />
-            ) : (
-                <HStack h="500px" alignItems="start" w="full">
-                    <ItemList
-                        isMarkerHovered={isMarkerHovered}
-                        itemHoveredId={itemHoveredId || ""}
-                        onHover={(id) => setItemHovered(id)}
-                        overflowY="scroll"
-                        columns={{ base: 1 }}
-                        page="events"
-                        events={events}
-                    />
-                    {events && events.length > 0 && (
-                        <Box minW="800px" h="500px">
-                            <EventsMap
-                                onMarkerHovered={(hov) => {
-                                    setIsMarkerHovered(hov);
-                                }}
-                                onHover={(id) => setItemHovered(id)}
-                                itemHoveredId={itemHoveredId || ""}
-                                events={events}
-                            />
-                        </Box>
-                    )}
-                </HStack>
-            )}
-        </Flex>
+                </Box>
+                {!isMapShowing ? (
+                    <ItemList page="events" events={events} />
+                ) : (
+                    <HStack h="500px" alignItems="start" w="full">
+                        <ItemList
+                            isMarkerHovered={isMarkerHovered}
+                            itemHoveredId={itemHoveredId || ""}
+                            onHover={(id) => setItemHovered(id)}
+                            overflowY="scroll"
+                            columns={{ base: 1 }}
+                            page="events"
+                            events={events}
+                        />
+                        {events && events.length > 0 && (
+                            <Box minW="800px" h="500px">
+                                <EventsMap
+                                    onMarkerHovered={(hov) => {
+                                        setIsMarkerHovered(hov);
+                                    }}
+                                    onHover={(id) => setItemHovered(id)}
+                                    itemHoveredId={itemHoveredId || ""}
+                                    events={events}
+                                />
+                            </Box>
+                        )}
+                    </HStack>
+                )}
+            </Flex>
+        </Suspense>
     );
 };
 
