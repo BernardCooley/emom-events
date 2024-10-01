dc6841ee1e08ca2fd970e426c117e239eba6a27b
bernard.cooley <bernard.cooley@global.com>
2024-10-01 23:44:07 +0100

    remove pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-    dest: "public",
-});
-
 /** @type {import('next').NextConfig} */
-
-export default withPWA({
+const nextConfig = {
     images: {
         domains: ["firebasestorage.googleapis.com"],
     },
-});
+};
+export default nextConfig;
